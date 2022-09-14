7fc766292c46a1eefd03d671966e69f652475872
韩毅 <2387870957@qq.com>
2022-09-14 17:36:24 +0800

    初始化项目

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld msg="Hello Vue 3.0 + Vite" />
+  <Hanyi />
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+import Hanyi from "./components/Hanyi.vue";
+
+export default {
+  name: "App",
+  components: {
+    HelloWorld,
+    Hanyi,
+  },
+};
+</script>
