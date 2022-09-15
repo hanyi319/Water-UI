1219ee254822e05585de9de2ce12a030808f644f
韩毅 <2387870957@qq.com>
2022-09-15 15:38:18 +0800

    实现 aside 切换

--- src/views/Doc.vue
+++ src/views/Doc.vue
@@ -1,7 +1,7 @@
 <template>
   <Topnav />
   <div class="content">
-    <aside>
+    <aside v-if="asideVisible">
       <h2>组件列表</h2>
       <ol>
         <li>
@@ -24,8 +24,15 @@
 
 <script lang="ts">
 import Topnav from "../components/Topnav.vue";
+import { inject, Ref } from "vue";
+
 export default {
   components: { Topnav },
+  setup() {
+    const asideVisible = inject<Ref<boolean>>("asideVisible");
+    console.log("Doc aside 获取到的 asideVisible 为：" + asideVisible.value);
+    return { asideVisible };
+  },
 };
 </script>
 
