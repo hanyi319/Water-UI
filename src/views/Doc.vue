7b8319857279cb90d0c49e03348d2e309d7aba88
韩毅 <2387870957@qq.com>
2022-09-15 14:36:17 +0800

    更新 aside 样式

--- src/views/Doc.vue
+++ src/views/Doc.vue
@@ -28,3 +28,23 @@ export default {
   components: { Topnav },
 };
 </script>
+
+<style lang="scss" scoped>
+aside {
+  background: #f9f9f9;
+  width: 150px;
+  padding: 16px;
+  position: fixed;
+  top: 0;
+  left: 0;
+  padding-top: 70px;
+  > h2 {
+    margin-bottom: 4px;
+  }
+  > ol {
+    > li {
+      padding: 4px 0;
+    }
+  }
+}
+</style>
