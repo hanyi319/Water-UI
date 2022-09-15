aa95b081f231861dcdd6af91ddb038f3fa1270cd
韩毅 <2387870957@qq.com>
2022-09-15 23:15:57 +0800

    完成 Switch 组件样式

---
+++ src/lib/Switch.vue
@@ -0,0 +1,25 @@
+<template>
+  <button><span></span></button>
+</template>
+
+<style lang="scss" scoped>
+$h: 22px;
+$h2: $h - 4px;
+button {
+  height: $h;
+  width: $h * 2;
+  border: none;
+  background: #42b883;
+  border-radius: $h / 2;
+  position: relative;
+}
+span {
+  position: absolute;
+  top: 2px;
+  left: 2px;
+  height: $h2;
+  width: $h2;
+  background: #fff;
+  border-radius: $h2 / 2;
+}
+</style>
