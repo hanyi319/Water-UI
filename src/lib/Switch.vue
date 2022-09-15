fbbbaef63cdcc54a4af94f39766504e0457eb466
韩毅 <2387870957@qq.com>
2022-09-15 23:25:01 +0800

    完成 Switch 切换功能

--- src/lib/Switch.vue
+++ src/lib/Switch.vue
@@ -1,6 +1,19 @@
 <template>
-  <button><span></span></button>
+  <button @click="toggle" :class="{ checked: checked }"><span></span></button>
 </template>
+
+<script lang="ts">
+import { ref } from "vue";
+export default {
+  setup() {
+    const checked = ref(false);
+    const toggle = () => {
+      checked.value = !checked.value;
+    };
+    return { checked, toggle };
+  },
+};
+</script>
 
 <style lang="scss" scoped>
 $h: 22px;
@@ -9,7 +22,7 @@ button {
   height: $h;
   width: $h * 2;
   border: none;
-  background: #42b883;
+  background: grey;
   border-radius: $h / 2;
   position: relative;
 }
@@ -21,5 +34,11 @@ span {
   width: $h2;
   background: #fff;
   border-radius: $h2 / 2;
+}
+button.checked {
+  background: #42b883;
+}
+button.checked > span {
+  left: calc(100% - #{$h2} - 2px);
 }
 </style>
