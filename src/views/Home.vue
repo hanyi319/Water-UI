09291f74876aa7eeb64777ed26dcf35fadb03405
韩毅 <2387870957@qq.com>
2022-09-15 14:07:52 +0800

    修改首页样式

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,11 +4,14 @@
     <div class="banner">
       <h1>清流 UI</h1>
       <h2>一个简洁的 UI 框架</h2>
-      <p class="actions">
-        <a href="">GitHub</a>
-        |
-        <a href="">开始</a>
-      </p>
+      <div class="actions">
+        <div class="action">
+          <a class="VPButton medium brand" href="">快速开始</a>
+        </div>
+        <div class="action">
+          <a class="VPButton medium alt" href="">查看源码</a>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -20,4 +23,47 @@ export default {
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.banner {
+  padding: 100px 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  background: #fff;
+  > .actions {
+    display: flex;
+    flex-wrap: wrap;
+    margin: -6px;
+    padding-top: 24px;
+    > .action {
+      flex-shrink: 0;
+      padding: 6px;
+      > .VPButton {
+        display: inline-block;
+        border: 1px solid transparent;
+        text-align: center;
+        font-weight: 500;
+        white-space: nowrap;
+        transition: color 0.25s, border-color 0.25s, background-color 0.25s;
+      }
+      > .medium {
+        border-radius: 20px;
+        padding: 0 20px;
+        line-height: 38px;
+        font-size: 14px;
+      }
+      > .brand {
+        border-color: #747bff;
+        color: rgba(255, 255, 255, 0.87);
+        background-color: #646cff;
+      }
+      > .alt {
+        border-color: #d1d1d1;
+        color: #213547;
+        background-color: #f2f2f2;
+      }
+    }
+  }
+}
+</style>
