44aefad65e838e412736e7a8fd9378b7cd39ce73
韩毅 <2387870957@qq.com>
2022-09-21 22:57:36 +0800

    优化 navItems

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -7,7 +7,7 @@
         v-for="(t, index) in titles"
         :ref="
           (el) => {
-            if (el) navItems[index] = el;
+            if (t === selected) selectedItem = el;
           }
         "
         :key="index"
@@ -40,21 +40,20 @@ export default {
     },
   },
   setup(props, context) {
-    // TypeScript 的泛型语法，声明一个 div 的数组
-    const navItems = ref<HTMLDivElement[]>([]);
+    // TypeScript 的泛型语法
+    const selectedItem = ref<HTMLDivElement>(null);
     const indicator = ref<HTMLDivElement>(null);
     const container = ref<HTMLDivElement>(null);
 
     const x = () => {
       // 动态设置 div 宽度
-      const divs = navItems.value;
-      const result = divs.filter((div) => div.classList.contains("selected"))[0];
-      const { width } = result.getBoundingClientRect();
+
+      const { width } = selectedItem.value.getBoundingClientRect();
       indicator.value.style.width = width + "px";
 
       // 动态设置 div 位置
       const { left: left1 } = container.value.getBoundingClientRect();
-      const { left: left2 } = result.getBoundingClientRect();
+      const { left: left2 } = selectedItem.value.getBoundingClientRect();
       const left = left2 - left1;
       indicator.value.style.left = left + "px";
     };
@@ -87,7 +86,7 @@ export default {
     const select = (title: string) => {
       context.emit("update:selected", title);
     };
-    return { navItems, indicator, container, defaults, titles, current, select };
+    return { selectedItem, indicator, container, defaults, titles, current, select };
   },
 };
 </script>
