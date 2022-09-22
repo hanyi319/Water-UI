683b4804d05421abd648e355a8e6d720bf6824d4
韩毅 <2387870957@qq.com>
2022-09-22 19:53:08 +0800

    填充文档内容

--- src/views/Install.vue
+++ src/views/Install.vue
@@ -1,3 +1,12 @@
 <template>
-  <div>安装</div>
+  <article>
+    <h1>安装</h1>
+    <p>打开终端运行以下命令：</p>
+    <pre><code>npm install toy-ui</code></pre>
+    <p>或</p>
+    <pre><code>yarn add toy-ui</code></pre>
+
+    <p>上一节：<a href="#/doc/intro">介绍</a></p>
+    <p>下一节：<a href="#/doc/get-started">开始</a></p>
+  </article>
 </template>
--- src/views/Intro.vue
+++ src/views/Intro.vue
@@ -1,3 +1,12 @@
 <template>
-  <div>Intro</div>
+  <article>
+    <h1>介绍</h1>
+    <p>Toy UI 是一款基于 Vue 3 和 TypeScript 的 UI 组件库。</p>
+    <p>该组件库旨在帮助新手学习，尽量不采用第三方库，包括这个官网也是自己编写的。</p>
+    <p>但是强烈不建议将此 UI 组件库用于生产环境。</p>
+    <p>不过，如果你是以学习源码为目的，那么这个库还是值得一看的，历史提交信息都很规范。</p>
+    <p>具体使用方法详见 README.md。</p>
+
+    <p>下一节：<a href="#/doc/install">安装</a></p>
+  </article>
 </template>
