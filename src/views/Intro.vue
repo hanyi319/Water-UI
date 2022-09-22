c4ecef5b0c7afc9aebfe73506054aab741d227a5
韩毅 <2387870957@qq.com>
2022-09-22 21:33:27 +0800

    支持 markdown 文件

--- src/views/Intro.vue
+++ src/views/Intro.vue
@@ -1,12 +1,13 @@
 <template>
-  <article class="markdown-body">
-    <h1>介绍</h1>
-    <p>Toy UI 是一款基于 Vue 3 和 TypeScript 的 UI 组件库。</p>
-    <p>该组件库旨在帮助新手学习，尽量不采用第三方库，包括这个官网也是自己编写的。</p>
-    <p>但是强烈不建议将此 UI 组件库用于生产环境。</p>
-    <p>不过，如果你是以学习源码为目的，那么这个库还是值得一看的，历史提交信息都很规范。</p>
-    <p>具体使用方法详见 README.md。</p>
-
-    <p>下一节：<a href="#/doc/install">安装</a></p>
-  </article>
+  <article class="markdown-body" v-html="md"></article>
 </template>
+
+<script>
+import { md } from "../markdown/intro.md";
+
+export default {
+  data() {
+    return { md };
+  },
+};
+</script>
