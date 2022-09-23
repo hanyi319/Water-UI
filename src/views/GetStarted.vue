083afe00b638946797fc22d4780b6c3340284e70
韩毅 <2387870957@qq.com>
2022-09-23 11:13:05 +0800

    添加其他 markdown 文件

--- src/views/GetStarted.vue
+++ src/views/GetStarted.vue
@@ -1,30 +1,13 @@
 <template>
-  <article class="markdown-body">
-    <h1>开始</h1>
-    <p>请先<a href="#/doc/install">安装</a>该组件库</p>
-    <p>然后在你的代码中引入</p>
-    <pre><code>import {Switch, Button, Dialog, Tabs} from "toy-ui"</code></pre>
-    <p>就可以正常使用了</p>
-
-    <h2>Vue 单文件组件</h2>
-    <p>代码示例：</p>
-    <pre><code>
-    &lt;template&gt;
-      &lt;div&gt;
-        &lt;Button&gt;按钮&lt;/Button&gt;
-      &lt;/div&gt;
-    &lt;/template&gt;
-
-    &lt;script&gt;
-    import {Switch, Button, Dialog, Tabs} from "toy-ui"
-      
-    export default {
-      components: {Button}
-    }
-    &lt;/script&gt;
-    </code></pre>
-
-    <p>上一节：<a href="#/doc/install">安装</a></p>
-    <p>下一节：<a href="#/doc/switch">Switch 开关</a></p>
-  </article>
+  <article class="markdown-body" v-html="md"></article>
 </template>
+
+<script>
+import md from "../markdown/get-started.md";
+
+export default {
+  data() {
+    return { md };
+  },
+};
+</script>
--- src/views/Install.vue
+++ src/views/Install.vue
@@ -1,12 +1,13 @@
 <template>
-  <article class="markdown-body">
-    <h1>安装</h1>
-    <p>打开终端运行以下命令：</p>
-    <pre><code>npm install toy-ui</code></pre>
-    <p>或</p>
-    <pre><code>yarn add toy-ui</code></pre>
-
-    <p>上一节：<a href="#/doc/intro">介绍</a></p>
-    <p>下一节：<a href="#/doc/get-started">开始</a></p>
-  </article>
+  <article class="markdown-body" v-html="md"></article>
 </template>
+
+<script>
+import md from "../markdown/install.md";
+
+export default {
+  data() {
+    return { md };
+  },
+};
+</script>
