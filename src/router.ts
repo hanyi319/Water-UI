327f8a957745b1ec811b9ee45cd76a2652ae09da
韩毅 <2387870957@qq.com>
2022-09-23 21:29:03 +0800

    封装 Markdown 组件

--- src/router.ts
+++ src/router.ts
@@ -2,15 +2,16 @@ import { createWebHashHistory, createRouter } from "vue-router";
 import Home from "./views/Home.vue";
 import Doc from "./views/Doc.vue";
 import DocDemo from "./components/DocDemo.vue";
-import Intro from "./views/Intro.vue";
-import GetStarted from "./views/GetStarted.vue";
-import Install from "./views/Install.vue";
 import ButtonDemo from "./components/ButtonDemo.vue";
 import SwitchDemo from "./components/SwitchDemo.vue";
 import DialogDemo from "./components/DialogDemo.vue";
 import TabsDemo from "./components/TabsDemo.vue";
+import Markdown from "./components/Markdown.vue";
+import { h } from "vue";
 
 const history = createWebHashHistory();
+const md = (filename) => h(Markdown, { path: `../markdown/${filename}.md`, key: filename });
+
 export const router = createRouter({
   history: history,
   routes: [
@@ -20,9 +21,9 @@ export const router = createRouter({
       component: Doc,
       children: [
         { path: "", component: DocDemo },
-        { path: "intro", component: Intro },
-        { path: "get-started", component: GetStarted },
-        { path: "install", component: Install },
+        { path: "intro", component: md("intro") },
+        { path: "install", component: md("install") },
+        { path: "get-started", component: md("get-started") },
         { path: "switch", component: SwitchDemo },
         { path: "button", component: ButtonDemo },
         { path: "dialog", component: DialogDemo },
