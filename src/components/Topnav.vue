d4077a270f0239ca737c4acbd4c97e432e758634
韩毅 <2387870957@qq.com>
2022-09-25 13:43:11 +0800

    优化 TopNav 组件

--- src/components/Topnav.vue
+++ src/components/Topnav.vue
@@ -1,18 +1,30 @@
 <template>
-  <div class="topnav">
-    <router-link to="/" class="logo">
-      <svg class="icon">
-        <use xlink:href="#icon-toy"></use>
+  <div class="header">
+    <div class="header-container">
+      <svg v-if="toggleMenuButtonVisible" class="toggleMenu" @click="toggleMenu">
+        <use xlink:href="#icon-menu"></use>
       </svg>
-    </router-link>
-    <ul class="menu">
-      <li>
-        <router-link to="/doc">文档</router-link>
-      </li>
-    </ul>
-    <svg v-if="toggleMenuButtonVisible" class="toggleMenu" @click="toggleMenu">
-      <use xlink:href="#icon-menu"></use>
-    </svg>
+      <router-link to="/" class="logo">
+        <svg class="icon">
+          <use xlink:href="#icon-toy"></use>
+        </svg>
+        <span class="can-hide">Toy UI</span>
+      </router-link>
+      <nav class="nav-links can-hide">
+        <div class="nav-item">
+          <router-link to="/doc/intro">指南</router-link>
+        </div>
+        <div class="nav-item">
+          <router-link to="/doc/switch">组件</router-link>
+        </div>
+        <div class="nav-item">
+          <a href="https://www.yuque.com/docs/share/784f9146-e690-4abc-bb08-22e570b23047?#">语雀</a>
+        </div>
+        <div class="nav-item">
+          <a href="https://github.com/hanyi319/Toy-UI">GitHub</a>
+        </div>
+      </nav>
+    </div>
   </div>
 </template>
 
@@ -37,55 +49,99 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-$color: #007974;
+$bg-primary: #fff;
+$border-primary: #e7e9e8;
 
-.topnav {
-  color: $color;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  padding: 16px;
+.header {
   position: fixed;
   top: 0;
   left: 0;
   width: 100%;
+  height: 60px;
   z-index: 10;
-  > .logo {
-    max-width: 6em;
-    margin-right: auto;
+  background: $bg-primary;
+  border-bottom: 1px solid $border-primary;
 
-    > svg {
-      width: 32px;
-      height: 32px;
-    }
-  }
-  > .menu {
+  > .header-container {
     display: flex;
-    white-space: nowrap;
-    flex-wrap: nowrap;
-    > li {
-      margin: 0 1em;
-    }
-  }
-  > .toggleMenu {
-    width: 24px;
-    height: 24px;
-    position: absolute;
-    left: 16px;
-    top: 50%;
-    transform: translateY(-50%);
-    display: none;
-  }
+    justify-content: center;
+    align-items: center;
+    height: 100%;
+    padding: 0 24px;
 
-  @media (max-width: 500px) {
-    > .menu {
+    > .logo {
+      display: inline-block;
+      font-weight: 500;
+      text-decoration: none;
+      margin-right: auto;
+
+      > svg {
+        height: 2.2rem;
+        min-width: 2.2rem;
+        margin-right: 0.8rem;
+        vertical-align: top;
+      }
+
+      > span {
+        font-size: 1.3rem;
+        font-weight: 600;
+        color: #2c3e50;
+        position: relative;
+      }
+    }
+
+    > .nav-links {
+      display: flex;
+      white-space: nowrap;
+
+      > .nav-item {
+        position: relative;
+        display: inline-block;
+        margin: 0 1rem;
+        line-height: 2rem;
+
+        > a {
+          line-height: 1.4rem;
+          color: inherit;
+          font-weight: 500;
+          text-decoration: none;
+        }
+
+        > .router-link-active {
+          color: #42b883;
+          font-weight: bolder;
+          margin-bottom: -2px;
+          border-bottom: 2px solid #42b883;
+        }
+      }
+    }
+
+    > .toggleMenu {
+      width: 24px;
+      height: 24px;
+      position: absolute;
+      left: 16px;
+      top: 50%;
+      transform: translateY(-50%);
       display: none;
     }
-    > .logo {
-      margin: 0 auto;
-    }
-    > .toggleMenu {
-      display: inline-block;
+
+    @media (max-width: 500px) {
+      > .logo .can-hide {
+        display: none;
+      }
+      > .nav-links .can-hide {
+        display: none;
+      }
+      > .menu {
+        display: none;
+      }
+      > .logo {
+        margin: 0 auto;
+      }
+      > .toggleMenu {
+        display: inline-block;
+      }
     }
   }
 }
